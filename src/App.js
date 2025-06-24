e9a57f53e19bd4912a4fa71ca863a8aab629a5f3
AaronGcM <acmedina@student.hau.edu.ph>
2025-06-24 18:14:21 +0800

    Update App.js
    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage'; 
+import LandingPage from './LandingPage';
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
